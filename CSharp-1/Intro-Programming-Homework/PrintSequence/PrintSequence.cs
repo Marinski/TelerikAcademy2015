3a789f98388e37d3de59218575893670373d5fdd
Marinski <marin_bs@abv.bg>
2015-01-09 01:17:50 +0200

    Connect Visual Studio and Git

---
+++ CSharp-1/Intro-Programming-Homework/PrintSequence/PrintSequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintSequence
+{
+    class PrintSequence
+    {
+        static void Main()
+        {
+            for (var i = 2; i < 12; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0},", i);
+                }
+                else
+                {
+                    Console.Write("{0},", -i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
