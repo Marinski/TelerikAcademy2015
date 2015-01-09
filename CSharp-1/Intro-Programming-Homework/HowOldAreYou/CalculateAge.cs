95e3f2992da1116ad03c5c81076d75e9094e3a19
Marinski <marin_bs@abv.bg>
2015-01-09 02:40:53 +0200

    Bonus Tasks Commit

---
+++ CSharp-1/Intro-Programming-Homework/HowOldAreYou/CalculateAge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CalculateAge
+{
+    class CalculateAge
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please type your age: ");
+            byte str = byte.Parse(Console.ReadLine());
+            Console.WriteLine("After 10 years you will be " + (str + 10) + " years old.");
+        }
+    }
+}
