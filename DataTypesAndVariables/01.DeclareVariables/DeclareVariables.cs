6f73e94c1310e8fbd3eb95a4e085f60252bc4831
Marinski <marin_bs@abv.bg>
2015-01-10 12:18:34 +0200

    First and Second Task - Primitive Data Types and Variables

---
+++ DataTypesAndVariables/01.DeclareVariables/DeclareVariables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class DeclareVariables
+{
+    static void Main()
+    {
+        ushort firstVal = 52130;
+        sbyte secondVal = -115;
+        int thirdVal = 4825932;
+        byte fourthVal = 97;
+        short fifthVal = -10000;
+
+        Console.WriteLine("Data type of {0} is ushort", firstVal);
+        Console.WriteLine("Data type of {0} is sbyte", secondVal);
+        Console.WriteLine("Data type of {0} is int", thirdVal);
+        Console.WriteLine("Data type of {0} is byte", fourthVal);
+        Console.WriteLine("Data type of {0} is short", fifthVal);
+    }
+}
---
+++ DataTypesAndVariables/02.FloatOrDouble/FloatOrDouble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class FloatOrDouble
+{
+    static void Main()
+    {
+        double firstVal = 34.567839023D;
+        float secondVal = 12.345F;
+        double thirdVal = 8923.1234857D;
+        float fourthVal = 3456.091F;
+
+        Console.WriteLine(firstVal);
+        Console.WriteLine(secondVal);
+        Console.WriteLine(thirdVal);
+        Console.WriteLine(fourthVal);
+    }
+}
