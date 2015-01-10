fa9b5783834e9877bc6e547ffcfec900c58c5b7e
Marinski <marin_bs@abv.bg>
2015-01-10 19:00:44 +0200

    commit tasks 8, 9, 10

---
+++ CSharp-1/DataTypesAndVariables/09.ExchangeVariableValues/ExchangeVariableValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ExchangeVariableValues
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+        string intro = "The values of 'a' and 'b' ";
+
+        Console.WriteLine(intro + "BEFORE exchange are: a = {0}, b = {1}", a, b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine(intro + "AFTER exchange are: a = {0}, b = {1}", a, b);
+    }
+}
---
+++ CSharp-1/DataTypesAndVariables/10.EmployeeData/EmployeeData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class EmployeeData
+{
+    static void Main()
+    {
+        string firstName = "Marin";
+        string lastName = "Stoyanov";
+        byte age = 26;
+        string gender = "m";
+        ulong personalID = 8901020304;
+        uint uniqueNum = 27899999;
+
+        Console.WriteLine("First Name: {0}\nLast Name: {1}\nAge: {2}\nGender: {3}\nPersonal ID number: {4}\nUnique employee number: {5}", firstName, lastName, age, gender, personalID, uniqueNum);
+    }
+}
