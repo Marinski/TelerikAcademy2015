77d09492dfba112115d1a12243fc43eaac825074
Marinski <marin_bs@abv.bg>
2015-01-10 13:09:35 +0200

    commit up to task 7

---
+++ CSharp-1/DataTypesAndVariables/05.BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class BooleanVariable
+{
+    static void Main()
+    {
+        bool isFemale = false;
+        Console.WriteLine("The author is female: {0}", isFemale);
+    }
+}
---
+++ CSharp-1/DataTypesAndVariables/06.StringsAndObjects/StringsAndObjects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class StringsAndObjects
+{
+    static void Main()
+    {
+        string firstVar = "Hello";
+        string secondVar = "World";
+        object concatenated = firstVar + " " + secondVar;
+        string thirdVar = (string)concatenated;
+
+        Console.WriteLine("{0} is now object type", concatenated);
+        Console.WriteLine("{0} is now string type", thirdVar);
+    }
+}
