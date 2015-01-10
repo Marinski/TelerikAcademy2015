2b433a0a6c069742ef61320fdd49b63d523add93
Marinski <marin_bs@abv.bg>
2015-01-10 12:52:09 +0200

    unicode character task

---
+++ CSharp-1/DataTypesAndVariables/04.UnicodeChar/UnicodeChar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class UnicodeChar
+{
+    static void Main()
+    {
+        char unicodeChar = '\u002A'; // Unicode code 42 (decimal) is star/ASTERISK/*
+
+        Console.WriteLine(unicodeChar);
+    }
+}
