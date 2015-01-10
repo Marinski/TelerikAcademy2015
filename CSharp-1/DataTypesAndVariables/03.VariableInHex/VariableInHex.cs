3f01bfb9ced00c8d7567f8ffd7a623bb1b471353
Marinski <marin_bs@abv.bg>
2015-01-10 12:36:27 +0200

    declare varaible in hexidecimal foramt

---
+++ CSharp-1/DataTypesAndVariables/03.VariableInHex/VariableInHex.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class VariableInHex
+{
+    static void Main()
+    {
+        int hexVar = 0xFE;
+        Console.WriteLine("Hexidecimal variable \"0xFE\" is {0} in decimal", hexVar);
+    }
+}
