433e3fa5ac767de510f02e642b0e3bd57eb797de
Marinski <marin_bs@abv.bg>
2015-01-10 19:04:50 +0200

    commit triangle task

---
+++ CSharp-1/DataTypesAndVariables/08.IsoscelesTriangle/IsoscelesTriangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class IsoscelesTriangle
+{
+    static void Main()
+    {
+        char copy = '\u00a9';
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("    " + copy + "\n");
+        Console.WriteLine("   " + copy + " " + copy + "\n");
+        Console.WriteLine("  " + copy + "   " + copy + "\n");
+        Console.WriteLine(" " + copy + " " + copy + " " + copy + " " + copy + "\n");
+    }
+}
