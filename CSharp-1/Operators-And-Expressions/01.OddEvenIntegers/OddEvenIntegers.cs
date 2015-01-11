3032abd0be673202dbb1f6d2d95ae90f2f030d25
Marinski <marin_bs@abv.bg>
2015-01-11 18:00:57 +0200

    program to check even or odd

---
+++ CSharp-1/Operators-And-Expressions/01.OddEvenIntegers/OddEvenIntegers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class OddEvenIntegers
+{
+    static void Main()
+    {
+        Console.Write("Enter number to check if EVER or ODD : ");
+        
+        int a = int.Parse(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("The number {0} is EVEN", a);
+        }
+        else
+        {
+            Console.WriteLine("The number {0} is ODD", a);
+        }
+    }
+}
