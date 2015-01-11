2e0e5d49f2ed39a8e39f97114c26dd997018342c
Marinski <marin_bs@abv.bg>
2015-01-11 19:23:23 +0200

    check if third digit is 7

---
+++ CSharp-1/Operators-And-Expressions/05.CheckThirdDigit/CheckThirdDigit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class CheckThirdDigit
+{
+    static void Main()
+    {
+        Console.Write("Enter number to check if third digit (RTL) is 7 : ");
+        int a = int.Parse(Console.ReadLine());
+
+        if (a < 100)
+        {
+            Console.WriteLine("Number you have entered has no 3rd digit");
+        }
+        else
+        {
+            if ( (a / 100) % 10 != 7)
+            {
+                Console.WriteLine("Third digit from right-to-left IS NOT 7.");
+            }
+            else
+            {
+                Console.WriteLine("Third digit from right-to-left IS 7.");
+            }
+        }
+    }
+}
