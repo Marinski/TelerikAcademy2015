1713c5c0f504141b5be5bf712fcf7f987551b9f4
Marinski <marin_bs@abv.bg>
2015-01-11 18:34:57 +0200

    commit task 3 and 4

---
+++ CSharp-1/Operators-And-Expressions/03.BooleanDivider/BooleanDivider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class BooleanDivider
+{
+    static void Main()
+    {
+        Console.Write("Enter number to check is it divided by 5 and 7 (without remainder): ");
+        int a = int.Parse(Console.ReadLine());
+        if (a % 5 == 0 && a % 7 == 0)
+        {
+            Console.WriteLine("{0} IS divided by 5 and 7 (without remainder)", a);
+        }
+        else
+        {
+            Console.WriteLine("{0} IS NOT divided by 5 and 7 (without remainder)", a);
+        }
+    }
+}
---
+++ CSharp-1/Operators-And-Expressions/04.Rectangles/CalcRectangles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class CalcRectangles
+{
+    static void Main()
+    {
+        float rectWidth = 3f;
+        float rectHeight = 4f;
+        float rectPerimeter = 2 * (rectWidth + rectHeight); // p = a + b + a + b = 2 * (a + b)
+        float rectArea = rectWidth * rectHeight; // s = a * b
+        Console.WriteLine("Rectangle with width {0} and height {1} has permeter {2} and area {3}", rectWidth, rectHeight, rectPerimeter, rectArea);
+
+        rectWidth = 2.5f;
+        rectHeight = 3f;
+        rectPerimeter = 2 * (rectWidth + rectHeight);
+        rectArea = rectWidth * rectHeight;
+        Console.WriteLine("Rectangle with width {0} and height {1} has permeter {2} and area {3}", rectWidth, rectHeight, rectPerimeter, rectArea);
+ 
+        rectWidth = 5f;
+        rectHeight = 5f;
+        rectPerimeter = 2 * (rectWidth + rectHeight);
+        rectArea = rectWidth * rectHeight;
+        Console.WriteLine("Rectangle with width {0} and height {1} has permeter {2} and area {3}", rectWidth, rectHeight, rectPerimeter, rectArea);
+    
+    }
+}
