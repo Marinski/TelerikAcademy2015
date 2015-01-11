4ebac6ca917f80e68cf624b8bf68ffb948a6235c
Marinski <marin_bs@abv.bg>
2015-01-11 18:14:59 +0200

    commit moon gravitation task

---
+++ CSharp-1/Operators-And-Expressions/02.MoonGravitation/MoonGravitation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MoonGravitation
+{
+    static void Main()
+    {
+        float gravField = 17 / 100f;
+        float testVal = 86f;
+        Console.WriteLine("Weight on Earth {0} = Weight on Moon {1}", testVal, testVal * gravField);
+        testVal = 74.6f;
+        Console.WriteLine("Weight on Earth {0} = Weight on Moon {1}", testVal, testVal * gravField);
+        testVal = 53.7f;
+        Console.WriteLine("Weight on Earth {0} = Weight on Moon {1}", testVal, testVal * gravField);
+    }
+}
