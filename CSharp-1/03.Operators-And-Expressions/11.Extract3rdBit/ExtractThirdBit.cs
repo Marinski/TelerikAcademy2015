25da2fde03172e7ae58055ce4501f71ceb76bf7a
Marinski <marin_bs@abv.bg>
2015-01-17 18:30:34 +0200

    Get the bit at position p=3 in a number n?

---
+++ CSharp-1/03.Operators-And-Expressions/11.Extract3rdBit/ExtractThirdBit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ExtractThirdBit
+{
+    static void Main()
+    {
+
+        int pos = 3;
+        Console.Write("Enter positive integer to check 3rd bit value : ");
+        uint n = Convert.ToUInt32(Console.ReadLine());  // 15 --> 00000000 00001111
+        int mask = 1 << pos;                                   // 00000000 01111000
+        uint nAndMask = (uint)(n & mask);                      // 00000000 01111111
+        uint bit = (uint)nAndMask >> pos;                      // 00000000 00001111
+        Console.WriteLine("3rd bit is --> {0}", bit);          // 1
+    }
+}
