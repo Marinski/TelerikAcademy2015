889ac4d6e9b503ab22a64d744ad675d99ded0435
Marinski <marin_bs@abv.bg>
2015-01-18 15:49:17 +0200

    Rename solution and add task 2

---
+++ CSharp-1/04.ConsoleInputOutput/02.PrintCompanyInfo/PrintCompanyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+class PrintCompanyInfo
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Console.OutputEncoding = Encoding.UTF8;
+        // Company info
+        Console.Write("Add Company Name...");
+        string coName = Console.ReadLine();
+        Console.Write("Add Company Address...");
+        string coAddress = Console.ReadLine();
+        Console.Write("Add Company Phone...");
+        string coPhone = Console.ReadLine();
+        Console.Write("Add Company Fax...");
+        string coFax = Console.ReadLine();
+        Console.Write("Add Company Website...");
+        string coWeb = Console.ReadLine();
+        // Manager info
+        Console.Write("Add Company Manager First Name...");
+        string mngrFirstName = Console.ReadLine();
+        Console.Write("Add Company Manager Last Name...");
+        string mngrLastName = Console.ReadLine();
+        Console.Write("Add Company Manager Age...");
+        string mngrAge = Console.ReadLine();
+        Console.Write("Add Company Manager Phone...");
+        string mngrPhone = Console.ReadLine();
+
+        Console.WriteLine("Your contact card looks good:\n");
+        Console.WriteLine(coName);
+        Console.WriteLine("Address: {0}", coAddress);
+        Console.WriteLine("Tel. {0}", coPhone);
+        Console.WriteLine("Fax: {0}", coFax);
+        Console.WriteLine("Web site: {0}", coWeb);
+        Console.WriteLine("Manager: {0} {1} (age: {2}, tel. {3})", mngrFirstName, mngrLastName, mngrAge, mngrPhone);
+    }
+}
