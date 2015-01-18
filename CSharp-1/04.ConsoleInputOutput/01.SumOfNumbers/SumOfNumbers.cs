c0c7f99c96ef442c6b497b9fbbeebe512ecd17c1
Marinski <marin_bs@abv.bg>
2015-01-18 15:28:13 +0200

    Begin Console Input/Output HW

---
+++ CSharp-1/04.ConsoleInputOutput/01.SumOfNumbers/SumOfNumbers.cs
@@ -0,0 +1,31 @@
+﻿// Write a program that reads 3 real numbers from the console and prints their sum.
+
+using System;
+using System.Threading;
+using System.Globalization;
+
+class SumOfNumbers
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+        Console.Write("Enter first number: ");
+        string first = Console.ReadLine();
+        first = first.Replace(",", ".");
+        double a = double.Parse(first);
+
+        Console.Write("Enter second number: ");
+        string second = Console.ReadLine();
+        second = second.Replace(",", ".");
+        double b = double.Parse(second);
+
+        Console.Write("Enter third number: ");
+        string third = Console.ReadLine();
+        third = third.Replace(",", ".");
+        double c = double.Parse(third);
+
+        double sum = a + b + c;
+        Console.WriteLine("Sum of numbers: {0} + {1} + {2} = {3}", a, b, c, sum);
+    }
+}
