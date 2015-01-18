ce8735cf2928c7cff08f539f8a6f0cf1991b79d1
Marinski <marin_bs@abv.bg>
2015-01-18 19:37:59 +0200

    task 5

---
+++ CSharp-1/04.ConsoleInputOutput/05.FormattingNumbers/FormattingNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+namespace _05.FormattingNumbers
+{
+    class FormattingNumbers
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int a = int.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            float c = float.Parse(Console.ReadLine());
+
+            // a in hex
+            // a in binary padded by zeros
+            // b print with 2 digits after decimal point, right aligned
+            // c print with 3 digits after the decimal poitn, left aligned
+
+            Console.WriteLine("{0,-10:X} | {1} | {2,10:F2} | {3,-10:F3} |", a, Convert.ToString(a, 2).PadLeft(10, '0'), b, c);
+        }
+    }
+}
