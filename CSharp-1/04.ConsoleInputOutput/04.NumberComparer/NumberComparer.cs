e373a3f7e86175b9b2f6faff8247e924db407e48
Marinski <marin_bs@abv.bg>
2015-01-18 19:14:11 +0200

    find bigger without a loop

---
+++ CSharp-1/04.ConsoleInputOutput/04.NumberComparer/NumberComparer.cs
@@ -0,0 +1,41 @@
+﻿//  Write a program that gets two numbers from the console and prints the greater of them.
+
+using System;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+class NumberComparer
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Enter first number: ");
+        string a = Console.ReadLine();
+        Console.Write("Enter second number: ");
+        string b = Console.ReadLine();
+
+        // Try to implement this without if statements
+        var max = Math.Max(Double.Parse(a), Double.Parse(b));
+        Console.WriteLine(max);
+
+        // Implement find bigger with if-else loop
+        // if (a != b)
+        // {
+        //     if (a > b)
+        //     {
+        //         Console.WriteLine("{0} > {1}", a, b);
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("{0} < {1}", a, b);
+        //     }
+        // }
+        // else
+        // {
+        //     Console.WriteLine("{0} = {1}", a, b);
+        // }
+    }
+}
