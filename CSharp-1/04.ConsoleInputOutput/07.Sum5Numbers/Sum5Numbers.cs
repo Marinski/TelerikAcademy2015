1d38ea45b959a200be202f68c0b301c6c5bef3c3
Marinski <marin_bs@abv.bg>
2015-01-18 20:51:24 +0200

    Commit sum of n numbers

---
+++ CSharp-1/04.ConsoleInputOutput/07.Sum5Numbers/Sum5Numbers.cs
@@ -0,0 +1,17 @@
+﻿// Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
+using System;
+
+class Sum5Numbers
+{
+    static void Main()
+    {
+        Console.Write("Enter five numbers (separated by a space): ");
+        string[] numbers = Console.ReadLine().Split(' ');
+        double sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += double.Parse(numbers[i]);
+        }
+        Console.WriteLine("\nSum of all numbers: {0}\n", sum);
+    }
+}
---
+++ CSharp-1/04.ConsoleInputOutput/08.NumbersFrom1ToN/PrintNumbersToN.cs
@@ -0,0 +1,17 @@
+﻿// Write a program that reads an integer number n from the console and prints all the numbers in the interval [1..n],
+// each on a single line.
+using System;
+
+class PrintNumbersToN
+{
+    static void Main()
+    {
+        Console.Write("Enter number 'n' to print numbers from 1 to n: ");
+        int n = int.Parse(Console.ReadLine());
+        
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
