6348bfbabde85bd3abff5a8d81e161b9fef1de82
Marinski <marin_bs@abv.bg>
2015-02-03 11:27:42 +0200

    CSharp-1 Exam Preparation

---
+++ CSharp-1/05.Conditional-Statements/04.MultiplicationSign/MultiplicationSign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Write a program that shows the sign (+, - or 0) of the product of three real numbers, without calculating it.
+// Use a sequence of if operators.
+
+class MultiplicationSign
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int sign = 0;
+
+        if (a == 0 || b == 0 || c == 0)
+        {
+            Console.WriteLine("The sign ot the product of your numbers is 0");
+        }
+        else
+        {
+            Console.WriteLine("The sign ot the products of your number is {0}", sign);
+        }
+    }
+}
---
+++ CSharp-1/Telerik Academy Exam 1 2013/1.2-4-8/2-4-8.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Numerics;
+
+
+namespace _1._2_4_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            //StreamReader reader = new StreamReader("..\\..\\input.txt");
+            //Console.SetIn(reader);
+
+            BigInteger a = int.Parse(Console.ReadLine());
+            BigInteger b = int.Parse(Console.ReadLine());
+            BigInteger c = int.Parse(Console.ReadLine());
+
+            BigInteger result = 0;
+            BigInteger r = 0;
+
+            if (b == 2)
+            {
+                r = a % c;
+            }
+            else if (b == 4)
+            {
+                r = a + c;
+            }
+            else if (b == 8)
+            {
+                r = a * c;
+            }
+
+            if (r % 4 == 0)
+            {
+                result = r / 4;
+            }
+            else
+            {
+                result = r % 4;
+            }
+            Console.WriteLine(result);
+            Console.WriteLine(r);
+        }
+    }
+}
