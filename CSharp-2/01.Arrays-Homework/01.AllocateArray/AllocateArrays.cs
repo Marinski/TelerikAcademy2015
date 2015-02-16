a807cd8f7871973d05d01f1826f8d9d981638b5d
Marinski <marin_bs@abv.bg>
2015-02-16 02:30:00 +0200

    do second task using method for compare

--- CSharp-2/01.Arrays-Homework/01.AllocateArray/AllocateArrays.cs
+++ CSharp-2/01.Arrays-Homework/01.AllocateArray/AllocateArrays.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Problem 1. Allocate array
+
+//    Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+//    Print the obtained array on the console.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +13,16 @@ namespace _01.AllocateArray
 {
     class AllocateArrays
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int count = 20;
+            int[] nums = new int[count];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = 5 * i;
+                Console.WriteLine("arr[{0}] = {1}", i, nums[i]);
+            }
         }
     }
 }
