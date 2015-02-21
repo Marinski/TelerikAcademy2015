3251d5f836896179d2107c47b9b555e6c731a976
Marinski <marin_bs@abv.bg>
2015-02-21 21:47:36 +0200

    SelectionSort C#

--- CSharp-2/01.Arrays-Homework/07.SelectionSort/SelectionSort.cs
+++ CSharp-2/01.Arrays-Homework/07.SelectionSort/SelectionSort.cs
@@ -3,7 +3,6 @@
 //    Sorting an array means to arrange its elements in increasing order. Write a program to sort an array.
 //    Use the Selection sort algorithm: Find the smallest element, move it at the first position, find the smallest from the rest, move it at the second position, etc.
 
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,9 +47,6 @@ namespace _07.SelectionSort
                 dataset[i] = dataset[min];
                 dataset[min] = temp;
             }
-
         }
-
-
     }
 }
