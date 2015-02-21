8e861fcf32ff90a2870cd222a67e2baf77ca544b
Marinski <marin_bs@abv.bg>
2015-02-21 19:27:10 +0200

    finds the sequence of max sum

---
+++ CSharp-2/01.Arrays-Homework/07.SelectionSort/SelectionSort.cs
@@ -0,0 +1,56 @@
+﻿//Problem 7. Selection sort
+
+//    Sorting an array means to arrange its elements in increasing order. Write a program to sort an array.
+//    Use the Selection sort algorithm: Find the smallest element, move it at the first position, find the smallest from the rest, move it at the second position, etc.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter array length: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] arr = new int[length]; //unsorted data set
+            for (int index = 0; index < length; index++)
+            {
+                Console.Write("Enter value for {0} : ", index + 1);
+                arr[index] = int.Parse(Console.ReadLine());
+            }
+
+            selectsort(arr, length); //sorting process using selection sort
+            int i;
+            for (i = 0; i < length; i++)
+            {
+                Console.Write(arr[i] + " "); //after sorting
+            }
+            Console.WriteLine();
+        }
+
+        ///selection sort
+        static void selectsort(int[] dataset, int n)
+        {
+            int i, j;
+            for (i = 0; i < n; i++)
+            {
+                int min = i;
+                for (j = i + 1; j < n; j++)
+                    if (dataset[j] < dataset[min]) min = j; //find min value
+                //then swap it with the beginning item of the unsorted list
+                int temp = dataset[i];
+                dataset[i] = dataset[min];
+                dataset[min] = temp;
+            }
+
+        }
+
+
+    }
+}
---
+++ CSharp-2/01.Arrays-Homework/08.MaximalSum/MaximalSum.cs
@@ -0,0 +1,88 @@
+﻿//Problem 8. Maximal sum
+
+//    Write a program that finds the sequence of maximal sum in given array.
+
+//Example:
+// input 	
+// 2, 3, -6, -1, 2, -1, 6, 4, -8, 8
+// result	
+// 2, -1, 6, 4
+
+//    Can you do it with only one loop (with single scan through the elements of the array)?
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _08.MaximalSum
+{
+    class MaximalSum
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("..\\..\\input.txt");
+            Console.SetIn(reader);
+            //input
+            Console.Write("Enter array size: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+
+            Console.WriteLine(size);// adds better visualisation while using StreamReader
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write("Enter element[{0}]: ", index);
+                array[index] = int.Parse(Console.ReadLine());
+                Console.WriteLine(array[index]);// adds better visualisation while using StreamReader
+            }
+
+            //check input
+            Console.Write("Array is [");
+            Console.Write(string.Join(", ", array));
+            Console.WriteLine("]");
+
+            //logic
+            int currSum = array[0];
+            int subSum = 0;
+            int maxSum = 0;
+            int currSequence = 1;
+            int maxSequence = 1;
+            int start = 0;
+            int tempStart = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                //subSum = currSum + array[i];
+                if (currSum + array[i] > array[i])
+                {
+                    currSum += array[i];
+                    currSequence++;
+                }
+                else
+                {
+                    currSum = array[i];
+                    tempStart = i;
+                    currSequence = 1;
+                }
+                if (currSum > maxSum)
+                {
+                    maxSum = currSum;
+                    start = tempStart;
+                    maxSequence = currSequence;
+                }
+            }
+
+            //output
+            Console.WriteLine("Maximal sequence sum is: {0}", maxSum);
+            Console.Write("Seqence consists: ");
+            for (int i = start; i < start + maxSequence; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
